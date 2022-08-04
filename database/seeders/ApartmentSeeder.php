a196347f25f7aa80dad4447a4b5a0d42aa2d99ba
Hamzah Al-Hagi <ha.773774142@gmail.com>
2022-08-04 03:31:04 +0300

    seeder

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Apartment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +15,20 @@ class ApartmentSeeder extends Seeder
     public function run()
     {
         //
+          //php artisan db:seed --class=ApartmentSeeder
+          Apartment::create([
+            'address' => 'جولة الكميم',
+                'size' => '32 متر مربع',
+                'apartment_type' => 'normal',
+                'rooms_number'=> 5,
+                'bathroms_number' => 2,
+                'apartment_description'=> 'تجاريه',
+                'start_at'=>  now(),
+                // 'end_at'=>   now(),
+                'rent_cyclic'=> 'monthly',
+                'price_of_rent'=> 20000.0,
+                'number_presenter_payment'=> 6,
+                'user_id'=> 1,
+            ]);
     }
 }
